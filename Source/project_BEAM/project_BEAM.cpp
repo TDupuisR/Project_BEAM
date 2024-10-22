4871180c5b14eafe2bdb66ec24feffcde2d91270
Nirgigo <titouan@dupuisland.net>
2024-10-22 10:42:09 +0100

    Initialisation

---
+++ Source/project_BEAM/project_BEAM.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "project_BEAM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_BEAM, "project_BEAM" );
+ 
