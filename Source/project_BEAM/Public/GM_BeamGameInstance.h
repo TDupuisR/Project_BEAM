90de8194a1385503c55224e59bb02246b2f20aed
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-11-13 11:09:12 +0100

    NEW : Blueprint callable for functions

--- Source/project_BEAM/Public/GM_BeamGameInstance.h
+++ Source/project_BEAM/Public/GM_BeamGameInstance.h
@@ -15,26 +15,37 @@ class PROJECT_BEAM_API UGM_BeamGameInstance : public UGameInstance
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable)
 	int GetManche();
 
+	UFUNCTION(BlueprintCallable)
 	void SetManche(int NewManche);
 
+	UFUNCTION(BlueprintCallable)
 	void AddManche();
 
+	UFUNCTION(BlueprintCallable)
 	int GetPlayerPoints(int PlayerIndex);
 
+	UFUNCTION(BlueprintCallable)
 	void SetPlayerPoints(int PlayerIndex, int NewPoints);
 
+	UFUNCTION(BlueprintCallable)
 	void AddPlayerPoints(int PlayerIndex, int Points);
 
+	UFUNCTION(BlueprintCallable)
 	void ResetPlayerPoints();
 
+	UFUNCTION(BlueprintCallable)
 	void SetPlayerPoints(TArray<int> NewPoints);
 
+	UFUNCTION(BlueprintCallable)
 	TArray<int> GetPlayerPoints();
 
+	UFUNCTION(BlueprintCallable)
 	void SetMaxManche(int NewMaxManche);
 
+	UFUNCTION(BlueprintCallable)
 	int GetMaxManche();
 
 private:
