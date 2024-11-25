6429f29a7e70ecda7b11d4bbb0cb74e852880c42
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-11-25 16:51:06 +0100

    NEW : Editor Tools to check spawns at the begin

--- Plugins/LocalMultiplayer/Source/LocalMultiplayer/LocalMultiplayer.Build.cs
+++ Plugins/LocalMultiplayer/Source/LocalMultiplayer/LocalMultiplayer.Build.cs
@@ -27,7 +27,8 @@ public class LocalMultiplayer : ModuleRules
 			{
 				"Core",
 				"EnhancedInput",
-				"InputCore"
+				"InputCore",
+                "UnrealEd",
 
 				// ... add other public dependencies that you statically link with here ...
 			}
--- Plugins/LocalMultiplayer/Source/LocalMultiplayer/Private/LocalMultiplayer.cpp
+++ Plugins/LocalMultiplayer/Source/LocalMultiplayer/Private/LocalMultiplayer.cpp
@@ -1,11 +1,30 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LocalMultiplayer.h"
+#include "Editor.h"
+
+#include "CoreMinimal.h"
+#include "SlateBasics.h"
+//#include "Slate/SlateBasics.h"
+#include "Framework/Notifications/NotificationManager.h"
+//#include "Notifications\SNotificationList.h"
+#include "Widgets/Notifications/SNotificationList.h"
+#include <Dialog/SMessageDialog.h>
+#include "Misc/MessageDialog.h"
+#include <Kismet/GameplayStatics.h>
+#include <Subsystems/EditorActorSubsystem.h>
+
 
 #define LOCTEXT_NAMESPACE "FLocalMultiplayerModule"
 
 void FLocalMultiplayerModule::StartupModule()
 {
+
+	UE_LOG(LogTemp, Warning, TEXT("LocalMultiplayer: Log Started"));
+
+	//FEditorDelegates::BeginPIE.AddUObject(this, &FLocalMultiplayerModule::OnBeginPIE);
+
+	
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	//FEditorDelegates::BeginPIE.AddUObject(this, &FLocalMultiplayerModule::OnBeginPIE);
 }
@@ -14,6 +33,11 @@ void FLocalMultiplayerModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+}
+
+void FLocalMultiplayerModule::OnBeginPIE(bool bIsSimulating)
+{
+	UE_LOG(LogTemp, Warning, TEXT("LocalMultiplayer: OnBeginPIE"));
 }
 
 #undef LOCTEXT_NAMESPACE
