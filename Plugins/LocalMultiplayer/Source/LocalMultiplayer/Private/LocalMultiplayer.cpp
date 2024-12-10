3b6f55d7232bdb517926078cf953ad7260ee158b
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-12-10 16:01:15 +0100

    FIX : fix pour le build

--- Plugins/LocalMultiplayer/Source/LocalMultiplayer/Private/LocalMultiplayer.cpp
+++ Plugins/LocalMultiplayer/Source/LocalMultiplayer/Private/LocalMultiplayer.cpp
@@ -1,18 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LocalMultiplayer.h"
-#include "Editor.h"
 
 #include "CoreMinimal.h"
-#include "SlateBasics.h"
-//#include "Slate/SlateBasics.h"
-#include "Framework/Notifications/NotificationManager.h"
-//#include "Notifications\SNotificationList.h"
-#include "Widgets/Notifications/SNotificationList.h"
-#include <Dialog/SMessageDialog.h>
-#include "Misc/MessageDialog.h"
-#include <Kismet/GameplayStatics.h>
-#include <Subsystems/EditorActorSubsystem.h>
 
 
 #define LOCTEXT_NAMESPACE "FLocalMultiplayerModule"
@@ -33,11 +23,6 @@ void FLocalMultiplayerModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
-}
-
-void FLocalMultiplayerModule::OnBeginPIE(bool bIsSimulating)
-{
-	UE_LOG(LogTemp, Warning, TEXT("LocalMultiplayer: OnBeginPIE"));
 }
 
 #undef LOCTEXT_NAMESPACE
