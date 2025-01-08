aed2fc423e4bbd199175e9199d3b13ec7ef4d2aa
Nirgigo <titouan@dupuisland.net>
2025-01-08 09:56:32 +0100

    ArmMovement correction

--- Source/project_BEAM/Private/Characters/PlayerAim.cpp
+++ Source/project_BEAM/Private/Characters/PlayerAim.cpp
@@ -49,10 +49,10 @@ FVector UPlayerAim::AimCursorPos(const FVector2D& dir, const FVector& playerPos,
 	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("DirNormal : %s"), *DirNormal.ToString()));
 	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("DirNormal : %d"), DirNormal.Length()));
 	
-	FVector TargetPos = FVector(playerPos.X + DirNormal.X * Radius, playerPos.Y, playerPos.Z + DirNormal.Y * Radius);
+	FVector TargetPos = FVector((playerPos.X + Character->GetVelocity().X) + DirNormal.X * Radius, playerPos.Y, (playerPos.Z + Character->GetVelocity().Z) + DirNormal.Y * Radius);
 	
-	//return FMath::VInterpTo(aimPos, TargetPos, DeltaTime, interpSpeed);
-	return TargetPos;
+	return FMath::VInterpTo(aimPos, TargetPos, DeltaTime, interpSpeed);
+	//return TargetPos;
 }
 
 void UPlayerAim::Shoot(FVector spawnLocation, FVector2D direction, int power)
